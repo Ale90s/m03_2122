b2217767a3b0b7757df991d7b6f6f0c4581c97e0
thiri <thiri@DESKTOP-MFPBPCQ>
2022-05-03 11:42:42 +0200

    Update

--- Acepta_El_Reto/src/acepta_el_reto/SinTerminar/EjAparcados/Ex167_Fractales.java
+++ Acepta_El_Reto/src/acepta_el_reto/SinTerminar/EjAparcados/Ex167_Fractales.java
@@ -7,26 +7,28 @@ public class Ex167_Fractales {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
-        
-        
-        
-        int longitudFractalMasGrande = in.nextInt(), sumatorio = 0, vueltaFractal = 0;
 
-        do {
+        for (int i = 0; i < 3; i++) {
 
-            if (vueltaFractal == 0) {
-                sumatorio += (longitudFractalMasGrande * 4);
-                // Primera vuelta, se multiplica el lado de un fractal por 4 lados (es un cuadrado)
-            } else {
-                sumatorio += (longitudFractalMasGrande * 4) * (Math.pow(4, vueltaFractal));
-                // Siguientes vueltas, se hace la misma multiplicación y a posteriori se eleva 
-                //4 cuadraditos que contiene cada fractal, empieza elevado a 1 y segun va subiendo el contador se va elevando a mas.
-            }
-            vueltaFractal++;
+            int longitudFractalMasGrande = in.nextInt(), sumatorio = 0, vueltaFractal = 0;
 
-            longitudFractalMasGrande /= 2;
+            do {
 
-        } while (longitudFractalMasGrande != 0);
-        System.out.println(sumatorio);
+                if (vueltaFractal == 0) {
+                    sumatorio += (longitudFractalMasGrande * 4);
+                    // Primera vuelta, se multiplica el lado de un fractal por 4 lados (es un cuadrado)
+                } else {
+                    sumatorio += (longitudFractalMasGrande * 4) * (Math.pow(4, vueltaFractal));
+                    // Siguientes vueltas, se hace la misma multiplicación y a posteriori se eleva 
+                    //4 cuadraditos que contiene cada fractal, empieza elevado a 1 y segun va subiendo el contador se va elevando a mas.
+                }
+                vueltaFractal++;
+
+                longitudFractalMasGrande /= 2;
+
+            } while (longitudFractalMasGrande != 0);
+            System.out.println(sumatorio);
+        }
+
     }
 }
