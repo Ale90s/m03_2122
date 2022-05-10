b53716b3e4a0a2432327b4171eb7be87c5cd100e
Ale Algarra <alejandroalgarrad@gmail.com>
2022-05-10 20:52:13 +0200

    update

--- UF3_Ejercicios/src/p1/test.java
+++ UF3_Ejercicios/src/p1/test.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class test {
     public static void main(String[] args) {
-
+        
         Scanner in = new Scanner(System.in);
-
+        
         System.out.println("Cuántas iteraciones?");
-
+        
         int iteraciones = in.nextInt();
-
+        
         for (int i = 0; i < iteraciones; i++) {
             System.out.println("test");
         }
