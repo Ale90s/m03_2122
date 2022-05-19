2610a22cc4e940d497bc6d9c381ff231e0332c0b
= <=>
2022-05-19 22:34:06 +0200

    update

--- UF4_Ejercicios/a2/lolEquipos2022.java
+++ UF4_Ejercicios/a2/lolEquipos2022.java
@@ -8,6 +8,8 @@ public class lolEquipos2022 {
         Competicion[] equipos = new Competicion[10];
 
         lectura(pathLectura, equipos);
+
+        imprimeEquipos(equipos);
     }
 
     public static void lectura(String path, Competicion[] equipos) throws IOException {
@@ -15,9 +17,10 @@ public class lolEquipos2022 {
         FileReader input = new FileReader(path);
         BufferedReader archivo = new BufferedReader(input);
         String linea;
+
         boolean equiposMetidos = false;
         String aux[];
-        int contador = 0;
+        int posicion = 0, posA, posB;
 
         while ((linea = archivo.readLine()) != null) {
 
@@ -26,13 +29,36 @@ public class lolEquipos2022 {
                 aux = linea.split("[:-]+");
 
                 if (equiposMetidos) {
+                    for (int i = 0; i < aux.length; i++) {
+                        if (aux[0].equals(equipos[i].getNomEquipos())) {
+                            posA = i;
+                        } else if (aux[2].equals(equipos[i].getNomEquipos())) {
+                            posB = i;
+                        }
+                    }
+                } else {
+                    equipos[posicion] = new Competicion();
+                    equipos[posicion].setNomEquipos(aux[0]);
+                    equipos[posicion].setTotal_punts(0);
+                    equipos[posicion].setTotal_Victories(0);
+                    equipos[posicion].setTotal_derrotes(0);
+                    posA = posicion;
+                    posicion++;
+                    equipos[posicion] = new Competicion();
+                    equipos[posicion].setNomEquipos(aux[2]);
+                    equipos[posicion].setTotal_punts(0);
+                    equipos[posicion].setTotal_Victories(0);
+                    equipos[posicion].setTotal_derrotes(0);
+                    posB = posicion;
+                    posicion++;
+                }
+
+                if (Integer.parseInt(aux[1]) > Integer.parseInt(aux[2])) {
 
                 } else {
-                    equipos[contador].setNomEquipos(aux[0]);
-                    contador++;
-                    equipos[contador].setNomEquipos(aux[0]);
-                    contador++;
+
                 }
+
             } else {
                 equiposMetidos = true;
             }
@@ -45,10 +71,11 @@ public class lolEquipos2022 {
         for (int i = 0; i < equipos.length; i++) {
 
             System.out.println("Nombre equipo: " + equipos[i].getNomEquipos());
-            /*System.out.println("Puntos: " + equipos[i].getTotal_punts());
-            System.out.println("Victorias: " + equipos[i].getTotal_Victories());
-            System.out.println("Derrotas: " + equipos[i].getTotal_derrotes());
-*/
+            /*
+             * System.out.println("Puntos: " + equipos[i].getTotal_punts());
+             * System.out.println("Victorias: " + equipos[i].getTotal_Victories());
+             * System.out.println("Derrotas: " + equipos[i].getTotal_derrotes());
+             */
         }
     }
 }
--- UF4_Ejercicios/a2/prueba2.java
+++ UF4_Ejercicios/a2/prueba2.java
@@ -1,7 +1,13 @@
 public class prueba2 {
     public static void main(String[] args) {
         
-        
-        System.out.println("test");
+        Competicion [] equipos = new Competicion[10];
+
+        equipos[0] = new Competicion();
+
+        equipos[0].setNomEquipos("nomEquipos");
+
+        System.out.println(equipos[0].getNomEquipos());
+
     }
 }
