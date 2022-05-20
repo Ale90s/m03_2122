66265a5750f1e472d6b5274a349e39cae4c7e0df
= <=>
2022-05-20 22:16:48 +0200

    update

--- UF4_Ejercicios/a2/lolEquipos2022.java
+++ UF4_Ejercicios/a2/lolEquipos2022.java
@@ -3,7 +3,7 @@ import java.io.*;
 public class lolEquipos2022 {
     public static void main(String[] args) throws IOException {
 
-        String pathLectura = "C:\\users\\aalgarra\\desktop\\resultatsLOL.txt";
+        String pathLectura = "C:\\thiri\\desktop\\resultatsLOL.txt";
         Competicion[] equipos = new Competicion[10];
 
         lectura(pathLectura, equipos);
