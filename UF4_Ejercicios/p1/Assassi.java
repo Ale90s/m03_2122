06cc922433452b204715f0fa89364cdf747a51aa
Ale90s <82205418+Ale90s@users.noreply.github.com>
2022-05-28 11:44:43 +0200

    Delete UF4_Ejercicios directory

--- UF4_Ejercicios/p1/Assassi.java
+++
@@ -1,9 +0,0 @@
-public class Assassi extends Personatges {
-
-    public Assassi(String nom, double forca, double constitucio, double velocitat, double inteligencia,
-            double sort) {
-
-        super(nom, forca, constitucio, velocitat, inteligencia, sort);
-
-    }
-}
